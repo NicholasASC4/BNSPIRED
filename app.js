5963b7f3c108bf204a778d8a3f7d47e57ce8a7e9
neilASC4 <cmantle721@gmail.com>
2017-08-10 11:14:41 -0400

    real

--- app.js
+++ app.js
@@ -17,16 +17,30 @@ $(document).ready(function(){
         var author = data.quoteAuthor
 
         $(".quotespot").empty();    
-        $(".quotespot").append("<div class='animated fadeIn'><h3>"+quote+"</h3></div>");
-        $(".authors").empty();
-        $(".authors").append("<div class='animated fadeIn'><h3>-"+author+"</h3></div>")
-
+        $(".quotespot").append("<div class='animated fadeIn'><h3>"+quote+" -"+author+"</h3></div>");
         if(author === ""){
-            $(".authors").html("<div class='animated fadeIn'><h3>-Unknown</h3></div>")
+            $(".quotespot").html("<div class='animated fadeIn'><h3>"+quote+" -Unknown</h3></div>");
         }
     }
 });
-
 });
+
+$('.btn').click(function(data){
+var clipboard = new Clipboard('.btn');
+
+clipboard.on('success', function(e) {
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+
+    e.clearSelection();
+});
+
+clipboard.on('error', function(e) {
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
+});
+});
+
 
 });
